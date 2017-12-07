9c9e2257636880953339703ff3f9039be236d03e
Loïc Knuchel <l.knuchel@criteo.com>
2017-12-07 10:30:58 +0100

    formation criteo day 3

--- src/main/scala/exercices/s3FunctionalTypes/CustomTry.scala
+++ src/main/scala/exercices/s3FunctionalTypes/CustomTry.scala
@@ -25,31 +25,31 @@ object CustomTry {
   }
 
   case class MySuccess[+A](value: A) extends MyTry[A] {
-    def isSuccess: Boolean = ???
-    def isFailure: Boolean = ???
-    def get: A = ???
-    def getOrElse[B >: A](default: => B): B = ???
-    def map[B](f: A => B): MyTry[B] = ???
-    def flatMap[B](f: A => MyTry[B]): MyTry[B] = ???
-    def filter(p: A => Boolean): MyTry[A] = ???
-    def foldLeft[B](z: B)(op: (B, A) => B): B = ???
-    def exists(p: A => Boolean): Boolean = ???
-    def toOption: Option[A] = ???
-    def toList: List[A] = ???
+    def isSuccess: Boolean = true
+    def isFailure: Boolean = false
+    def get: A = value
+    def getOrElse[B >: A](default: => B): B = value
+    def map[B](f: A => B): MyTry[B] = MyTry(f(value))
+    def flatMap[B](f: A => MyTry[B]): MyTry[B] = f(value)
+    def filter(p: A => Boolean): MyTry[A] = if(p(value)) this else MyFailure(new Exception)
+    def foldLeft[B](z: B)(op: (B, A) => B): B = op(z, value)
+    def exists(p: A => Boolean): Boolean = p(value)
+    def toOption: Option[A] = Some(value)
+    def toList: List[A] = List(value)
   }
 
   case class MyFailure[+A](error: Throwable) extends MyTry[A] {
-    def isSuccess: Boolean = ???
-    def isFailure: Boolean = ???
-    def get: A = ???
-    def getOrElse[B >: A](default: => B): B = ???
-    def map[B](f: A => B): MyTry[B] = ???
-    def flatMap[B](f: A => MyTry[B]): MyTry[B] = ???
-    def filter(p: A => Boolean): MyTry[A] = ???
-    def foldLeft[B](z: B)(op: (B, A) => B): B = ???
-    def exists(p: A => Boolean): Boolean = ???
-    def toOption: Option[A] = ???
-    def toList: List[A] = ???
+    def isSuccess: Boolean = false
+    def isFailure: Boolean = true
+    def get: A = throw error
+    def getOrElse[B >: A](default: => B): B = default
+    def map[B](f: A => B): MyTry[B] = MyFailure(error)
+    def flatMap[B](f: A => MyTry[B]): MyTry[B] = MyFailure(error)
+    def filter(p: A => Boolean): MyTry[A] = this
+    def foldLeft[B](z: B)(op: (B, A) => B): B = z
+    def exists(p: A => Boolean): Boolean = false
+    def toOption: Option[A] = None
+    def toList: List[A] = List.empty
   }
 
 }
--- src/test/scala/corrections/Correction20171204.scala
+++ src/test/scala/corrections/Correction20171204.scala
@@ -2,6 +2,8 @@ package corrections
 
 import org.scalatest.{FunSpec, Matchers}
 
+import scala.util.control.NonFatal
+
 class Correction20171204 extends FunSpec with Matchers {
   describe("FizzBuzz") {
     def fizzbuzz(i: Int): String = {
@@ -128,6 +130,57 @@ class Correction20171204 extends FunSpec with Matchers {
         slots.length shouldBe 234
         rooms.length shouldBe 18
       }
+    }
+  }
+
+  describe("CustomTry") {
+    sealed abstract class MyTry[+A] {
+      def isSuccess: Boolean
+      def isFailure: Boolean
+      def get: A
+      def getOrElse[B >: A](default: => B): B
+      def map[B](f: A => B): MyTry[B]
+      def flatMap[B](f: A => MyTry[B]): MyTry[B]
+      def filter(p: A => Boolean): MyTry[A]
+      def foldLeft[B](z: B)(op: (B, A) => B): B
+      def exists(p: A => Boolean): Boolean
+      def toOption: Option[A]
+      def toList: List[A]
+    }
+
+    object MyTry {
+      def apply[A](v: => A): MyTry[A] =
+        try MySuccess(v) catch {
+          case NonFatal(e) => MyFailure(e)
+        }
+    }
+
+    case class MySuccess[+A](value: A) extends MyTry[A] {
+      def isSuccess: Boolean = true
+      def isFailure: Boolean = false
+      def get: A = value
+      def getOrElse[B >: A](default: => B): B = value
+      def map[B](f: A => B): MyTry[B] = MyTry(f(value))
+      def flatMap[B](f: A => MyTry[B]): MyTry[B] = f(value)
+      def filter(p: A => Boolean): MyTry[A] = if(p(value)) this else MyFailure(new Exception)
+      def foldLeft[B](z: B)(op: (B, A) => B): B = op(z, value)
+      def exists(p: A => Boolean): Boolean = p(value)
+      def toOption: Option[A] = Some(value)
+      def toList: List[A] = List(value)
+    }
+
+    case class MyFailure[+A](error: Throwable) extends MyTry[A] {
+      def isSuccess: Boolean = false
+      def isFailure: Boolean = true
+      def get: A = throw error
+      def getOrElse[B >: A](default: => B): B = default
+      def map[B](f: A => B): MyTry[B] = MyFailure(error)
+      def flatMap[B](f: A => MyTry[B]): MyTry[B] = MyFailure(error)
+      def filter(p: A => Boolean): MyTry[A] = this
+      def foldLeft[B](z: B)(op: (B, A) => B): B = z
+      def exists(p: A => Boolean): Boolean = false
+      def toOption: Option[A] = None
+      def toList: List[A] = List.empty
     }
   }
 }
