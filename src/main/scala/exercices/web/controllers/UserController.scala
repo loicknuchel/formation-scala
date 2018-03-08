b9d51f2ede4f7d7f0f4c6a79854738dc3973c5bb
Loïc Knuchel <l.knuchel@criteo.com>
2018-03-08 21:27:41 +0100

    day 3

--- src/main/scala/exercices/web/controllers/UserController.scala
+++ src/main/scala/exercices/web/controllers/UserController.scala
@@ -9,7 +9,7 @@ import exercices.web.utils.Extensions._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.Try
+import scala.util.{Failure, Success, Try}
 
 class UserController(store: KeyValueStore[User.Id, User], mapper: FinatraObjectMapper) extends Controller {
   /**
@@ -19,7 +19,20 @@ class UserController(store: KeyValueStore[User.Id, User], mapper: FinatraObjectM
     * Tip2: finatra controllers want twitter Future instead of scala Future, you should transform them (see utils.Extensions)
     */
   get("/api/users") { _: Request =>
-    ???
+    val res: Future[Seq[User]] = store.keys().flatMap { ids: Seq[User.Id] =>
+      val a: Seq[Future[Option[User]]] = ids.map(id => store.read(id))
+      val b: Future[Seq[Option[User]]] = Future.sequence(a)
+      b
+    }.map { userOptions: Seq[Option[User]] =>
+      userOptions.flatten
+    }
+
+    val res1: Future[Seq[User]] = for {
+      ids <- store.keys()
+      users <- Future.sequence(ids.map(store.read))
+    } yield users.flatten
+
+    res1.map(users => response.ok.json(users)).toTwitter.rescue(handleError)
   }
 
   /**
@@ -28,7 +41,26 @@ class UserController(store: KeyValueStore[User.Id, User], mapper: FinatraObjectM
     * Tip: FinatraObjectMapper is able to parse String into case classes
     */
   post("/api/users") { req: Request =>
-    ???
+    /*val user = userNoId.generate
+    val res = store.create(user.id, user).map(_ => user.id)
+    res.map(response.ok.json).toTwitter.rescue(handleError)*/
+
+    /*val userTry = Try(mapper.parse[UserNoId](req.contentString))
+    val id = User.Id.generate()
+    userTry match {
+      case Success(user) =>
+        store.create(id, User(id, user.firstName, user.lastName)).map { _ =>
+          response.ok.json(id)
+        }.toTwitter.rescue(handleError)
+      case Failure(e) => response.internalServerError.toFuture
+    }*/
+
+    val res: Future[User.Id] = for {
+      userNoId <- Try(mapper.parse[UserNoId](req.contentString)).toFuture
+      user = userNoId.generate
+      _ <- store.create(user.id, user)
+    } yield user.id
+    res.map(response.ok.json).toTwitter.rescue(handleError)
   }
 
   /**
@@ -37,20 +69,37 @@ class UserController(store: KeyValueStore[User.Id, User], mapper: FinatraObjectM
     * Tip: look at 'Request' methods
     */
   get("/api/users/:id") { req: Request =>
-    ???
+    val res: Future[Option[User]] = for {
+      id <- Try(User.Id(req.getParam("id"))).toFuture
+      userOpt <- store.read(id)
+    } yield userOpt
+
+    res.map {
+      case Some(user) => response.ok.json(user)
+      case None => response.notFound
+    }.toTwitter.rescue(handleError)
   }
 
   /**
     * Endpoint to update an existing user
     */
   put("/api/users/:id") { req: Request =>
-    ???
+    val res: Future[User.Id] = for {
+      id <- Try(User.Id(req.getParam("id"))).toFuture
+      userNoId <- Try(mapper.parse[UserNoId](req.contentString)).toFuture
+      _ <- store.update(id, userNoId.withId(id))
+    } yield id
+    res.map(response.ok.json).toTwitter.rescue(handleError)
   }
 
   /**
     * Endpoint to delete a user
     */
   delete("/api/users/:id") { req: Request =>
-    ???
+    val res: Future[User.Id] = for {
+      id <- Try(User.Id(req.getParam("id"))).toFuture
+      _ <- store.delete(id)
+    } yield id
+    res.map(response.ok.json).toTwitter.rescue(handleError)
   }
 }
